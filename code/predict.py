f5fe4a261d90fe32af7fec0fa62af382ef4ffb3b
Jocelyn <jocelynrachel2022@gmail.com>
2026-01-28 00:32:25 +0530

    chore: remove legacy code folder

--- code/predict.py
+++
@@ -1,5 +0,0 @@
-from college_enquiry_chatbot.cli import chat_command
-
-
-if __name__ == "__main__":
-    raise SystemExit(chat_command(None))
--- code/train.py
+++
@@ -1,5 +0,0 @@
-from college_enquiry_chatbot.cli import train_command
-
-
-if __name__ == "__main__":
-    raise SystemExit(train_command())
